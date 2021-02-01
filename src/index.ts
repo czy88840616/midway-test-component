756ba4bb2c7197b168061e2c0d68a0a28e987b40
Harry Chen <czy88840616@gmail.com>
2021-02-01 18:38:39 +0800

    chore: code add

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { AutoConfiguration as Configuration } from './configuration';
+export * from './service/bookService';
+export * from './controller/testController';
+export * from './schedule/report';
